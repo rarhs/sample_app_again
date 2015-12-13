064ff122ee939f477ec0927fbb7e73545e551595
rarhs <reza054@gmail.com>
2015-12-13 20:08:01 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  # Not the final implementation
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user
     else
       render 'new'
     end
   end
-    
+  
     private
     
       def user_params
