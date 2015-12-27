489eb8109d9a10c97bad15531b060b504eb22fb2
rarhs <reza054@gmail.com>
2015-12-27 12:30:27 +0000

    Solves exercise 11.2

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   
+  include ActionView::Helpers::TextHelper
+  
   def setup
     @user = users(:michael)
   end
@@ -32,5 +34,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{pluralize(@user.microposts.count, 'micropost')}", response.body
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "#{pluralize(other_user.microposts.count, 'micropost')}", response.body
   end
 end
