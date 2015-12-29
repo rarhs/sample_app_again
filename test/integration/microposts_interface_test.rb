707d3534494e450c44dd3e38512313458d0965f1
rarhs <reza054@gmail.com>
2015-12-29 08:14:06 +0000

    Solves exercise 11.3

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,10 +8,13 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
+  
+  
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: {content: ""}
@@ -19,10 +22,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: {content: content}
+      post microposts_path, micropost: {content: content, picture: picture}
     end
-    assert_redirected_to root_url
+    micropost = assigns(:micropost)
+    assert micropost.picture?
     follow_redirect!
     assert_match content, response.body
     # Delete a post.
